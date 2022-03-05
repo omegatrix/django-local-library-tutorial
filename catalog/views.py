84707c9a9186d3566492c190db8be31621210bc8
Arnold Anthonypillai <arnoldbronson16@gmail.com>
2022-03-05 16:21:49 +0000

    Add book_detail & book_list templates

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 
 from .models import Author, Book, BookInstance, Genre
 
@@ -18,6 +19,9 @@ def index(request):
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
     num_genres = Genre.objects.count()
+    num_visits = request.session.get("num_visits", 0)
+
+    request.session["num_visits"] = num_visits + 1
 
     context = {
         "page_title": "Local library",
@@ -26,6 +30,7 @@ def index(request):
         "num_instances_available": num_instances_available,
         "num_authors": num_authors,
         "num_genres": num_genres,
+        "num_visits": num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
@@ -34,3 +39,19 @@ def index(request):
         "index.html",
         context=context,
     )
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get the context
+        context = super(BookListView, self).get_context_data(**kwargs)
+        # Create any data and add it to the context
+        context["some_data"] = "This is just some data"
+        return context
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
